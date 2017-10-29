e4cf5f16613eeefbdd2ec325e069350cf522f6ef
Raselahmed <raselahmed825@gmail.com>
2017-10-29 20:30:23 +0000

    assignment9

--- routes/article.server.routes.js
+++ routes/article.server.routes.js
@@ -3,20 +3,18 @@ module.exports = function(app){
  var articles = require('./../controllers/articles.server.controller.js');
  var users = require('./../controllers/users.server.controller.js');
 
-
-app.route('/articles/new')
-.get(articles.new);
-
-app.route('/articles/all')
+ app.route('/articles/new')
+    .get(articles.new);
+    
+ app.route('/articles/all')
     .get(articles.all);
     
-app.route('/articles/:articleId')
+ app.route('/articles/:articleId')
     .get(articles.view);
-
-app.route('/articles/edit/:articleId')
+    
+ app.route('/articles/edit/:articleId')
     .get(articles.edit);
-
-
+ 
  app.route('/api/articles')
 	.get(articles.list)
 	.post(users.requiresLogin, articles.create);
@@ -28,52 +26,9 @@ app.route('/articles/edit/:articleId')
 	app.route('/api/articles/edit/:articleId')
 	.get(articles.read)
 	.put(users.requiresLogin, articles.update);
-	
 
 
 app.param('articleId', articles.articleByID);
 
 
 }
-
-exports.articleByID = function(req, res,next,id){
-    Article.findById(id).populate('user','email').exex(function(err,article){
-        if(err)return next(err);
-        if(!article) return next (new Error('Failed to load article ' + id));
-        req.article = article;
-        next();
-        
-    });
-};
-
-exports.view = function (req,res) {
-        res.render('./../views/article/view.ejs',{
-            user:req.user || null,
-            request:req
-        });
-};
-
-exports.edit =function(req,res){
-    res.render('./../public/views/article/edit.ejs')
-}
-
-
-exports.all = function(req,res){
-    Article.find(function(err,data){
-    if(err){
-        return res.status(400).send({
-            message: errorHandler.getErrorMessage(err)
-        });
-    }else{
-        console.log("api called");
-        res.render('./../public/views/article/all.ejs',{
-            user: req.user || null,
-            request: req,
-            articles: data
-        });
-    }
-        
-    });
-};
-
-
